58334833460e3379489ae7c76ceccae55298fb78
Aaron-Reyes-Rodriguez <aaronreyesrodriguez504@gmail.com>
2026-02-10 18:26:47 -0800

    Fixed error "'getProfessor' is declared but its value is never read" for Frontend Delpoyment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { apiBase, auditCs, fetchHealth, getClass, getProfessor} from './api'
+import { apiBase, auditCs, fetchHealth, getClass} from './api'
 import FileUploader from './api'
 
 
