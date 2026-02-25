08c22795d18e91429c9b37d6ca22a3d5b26fa69e
Aaron-Reyes-Rodriguez <aaronreyesrodriguez504@gmail.com>
2026-02-25 11:21:01 -0800

    Take out unecessary home page things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { fetchHealth, getClass } from './api'
+import { fetchHealth } from './api'
 
 
 function App() {
   const [health, setHealth] = useState<string | null>(null)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<string | null>(null)
-  const [classData, setClassData] = useState<unknown>(null)
-  const [classId, setClassId] = useState<string | null>(null)
+  // const [classData, setClassData] = useState<unknown>(null)
+  // const [classId, setClassId] = useState<string | null>(null)
 
   
-  async function getClassData(classId: string) {
-    console.log(classId)
-    const data = await getClass(classId)
-    setClassData(JSON.stringify(data))
-    console.log(classData)
-  }
+  // async function getClassData(classId: string) {
+  //   console.log(classId)
+  //   const data = await getClass(classId)
+  //   setClassData(JSON.stringify(data))
+  //   console.log(classData)
+  // }
 
-  const handleClassData = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // 2. Update the state with the current text box value
-    setClassId(event.target.value);
-  };
+  // const handleClassData = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   // 2. Update the state with the current text box value
+  //   setClassId(event.target.value);
+  // };
 
   async function checkBackend() {
     setError(null)
@@ -42,14 +40,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Quackademics</h1>
       <div className="card">
       </div>
@@ -65,19 +56,6 @@ function App() {
       </div>
 
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      
-      <div className="card"></div>
-        <input 
-          type="text" 
-          value={classId || ''} // Binds the box to the state
-          onChange={handleClassData} // Fires on every keystroke
-        />
-        <button onClick={() => getClassData(classId || '')} disabled={loading || !classId}>
-          Get Class Data
-        </button>
     </>
   )
 }
