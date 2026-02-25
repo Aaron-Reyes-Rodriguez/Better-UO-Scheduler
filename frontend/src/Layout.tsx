8ba967db8340e57cdeab4f75b24e85e7b5d9a700
Aaron-Reyes-Rodriguez <aaronreyesrodriguez504@gmail.com>
2026-02-25 13:11:55 -0800

    NavBar look updated

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,13 +1,24 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './pages/components/navbar';
+import Box from '@mui/material/Box';
 
 export default function Layout() {
     return (
-        <div>
+        <Box 
+            sx={{ 
+                position: 'absolute', 
+                top: 0, 
+                left: 0, 
+                right: 0, 
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+        >
             <Navbar />
-            <main>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 <Outlet />
-            </main>
-        </div>
+            </Box>
+        </Box>
     );
 }
